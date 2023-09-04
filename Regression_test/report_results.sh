ceebb3dd89d00a99f2b70d8d6750cf1f34b44911
Jiahui Xu <jxu@ethz.ch>
2023-09-04 21:09:25 +0200

    update regression test report generation script

--- Regression_test/report_results.sh
+++ Regression_test/report_results.sh
@@ -6,17 +6,30 @@ trap "exit" INT
 # directory while the script is
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "List of passed benchmarks"
-find . -name 'hls_verify.log' -exec \
-	bash -c "grep -q 'Comparison of.*Pass' {} && echo {}' result(s) ok!'"  \;
-echo "------------------------------------"
+cd $SCRIPT_DIR
 
-echo "List of failed benchmarks"
-find . -name 'hls_verify.log' -exec \
-	bash -c "grep -q 'Comparison of.*Fail' {} && echo {}' result(s) incorrect!'"  \;
-echo "------------------------------------"
+(
+	echo "List of passed benchmarks"
+	find . -name 'hls_verify.log' -exec \
+		bash -c "grep -q 'Comparison of.*Pass' {} && echo {}' result(s) ok!'"  \;
+	echo "------------------------------------"
 
-echo 'List of missing benchmarks'
-(cd examples; find . -maxdepth 1 -type d -not -path '.' \
-	-exec bash -c "[ -f {}/reports/hls_verify.log ] || echo {}' not synthesized'" \;)
-echo "------------------------------------"
+	echo "List of failed benchmarks"
+	find . -name 'hls_verify.log' -exec \
+		bash -c "grep -q 'Comparison of.*Fail' {} && echo {}' result(s) incorrect!'"  \;
+
+	find . -name 'hls_verify.log' -exec \
+		bash -c "grep -q 'Comparison of.*' {} || echo {}' result(s) missing!'"  \;
+	echo "------------------------------------"
+
+	echo 'List of missing benchmarks'
+	(cd examples; find . -maxdepth 1 -type d -not -path '.' \
+		-exec bash -c "[ -f {}/reports/hls_verify.log ] || echo {}' not tested yet!'" \;)
+	echo "------------------------------------"
+
+	echo 'MILP Runtimes'
+	find . -name 'buffers.log' -exec \
+		grep -H 'Total MILP' {} \;
+
+
+) | tee results.log
