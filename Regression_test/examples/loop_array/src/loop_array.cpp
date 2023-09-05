a22bbb69687f7c90a76a364391e50d49decc1f64
Jiahui Xu <jxu@ethz.ch>
2023-09-05 10:51:27 +0200

    [Regression Test] fixing the loop_array example (too few iterations for Buffer)

--- Regression_test/examples/loop_array/src/loop_array.cpp
+++ Regression_test/examples/loop_array/src/loop_array.cpp
@@ -3,8 +3,9 @@
 
 #define AMOUNT_OF_TEST 1
 
-void loop_array(in_int_t n, in_int_t k, inout_int_t c[10]) {
-	
+
+void loop_array(in_int_t n, in_int_t k, inout_int_t c[LOOP_BOUND]) {
+
 	for (int i = 1; i < n; i++) {
 		c[i] = k + c[i-1];
 	}
@@ -13,21 +14,20 @@ void loop_array(in_int_t n, in_int_t k, inout_int_t c[10]) {
 int main(void){
 	in_int_t k[AMOUNT_OF_TEST];
 	in_int_t n[AMOUNT_OF_TEST];
-	inout_int_t c[AMOUNT_OF_TEST][10];
-    
+	inout_int_t c[AMOUNT_OF_TEST][LOOP_BOUND];
+
 	srand(13);
 	for(int i = 0; i < AMOUNT_OF_TEST; ++i){
-		k[i] = rand()%10;
-		n[i] = rand()%10;
-	        for(int j = 0; j < 10; ++j){
-		   c[i][j] = 0;
+		k[i] = rand() % LOOP_BOUND;
+		n[i] = rand() % LOOP_BOUND;
+		for(int j = 0; j < LOOP_BOUND; ++j){
+			c[i][j] = 0;
+		}
 	}
-                
-    }
-	
+
 
 	//for(int i = 0; i < AMOUNT_OF_TEST; ++i){
-    int i = 0;
+	int i = 0;
 	loop_array(n[i], k[i], c[i]);
 	//}
 }
