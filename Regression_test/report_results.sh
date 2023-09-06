b7148a47a04ff97d253b02794876deed6e90b7af
Jiahui Xu <jxu@ethz.ch>
2023-09-06 11:28:30 +0200

    [Regression Test] report_results.sh add sort output

--- Regression_test/report_results.sh
+++ Regression_test/report_results.sh
@@ -11,25 +11,25 @@ cd $SCRIPT_DIR
 (
 	echo "List of passed benchmarks"
 	find . -name 'hls_verify.log' -exec \
-		bash -c "grep -q 'Comparison of.*Pass' {} && echo {}' result(s) ok!'"  \;
+		bash -c "grep -q 'Comparison of.*Pass' {} && echo {}' result(s) ok!'"  \; | sort
 	echo "------------------------------------"
 
 	echo "List of failed benchmarks"
 	find . -name 'hls_verify.log' -exec \
-		bash -c "grep -q 'Comparison of.*Fail' {} && echo {}' result(s) incorrect!'"  \;
+		bash -c "grep -q 'Comparison of.*Fail' {} && echo {}' result(s) incorrect!'"  \; | sort
 
 	find . -name 'hls_verify.log' -exec \
-		bash -c "grep -q 'Comparison of.*' {} || echo {}' result(s) missing!'"  \;
+		bash -c "grep -q 'Comparison of.*' {} || echo {}' result(s) missing!'"  \; | sort
 	echo "------------------------------------"
 
 	echo 'List of missing benchmarks'
 	(cd examples; find . -maxdepth 1 -type d -not -path '.' \
-		-exec bash -c "[ -f {}/reports/hls_verify.log ] || echo {}' not tested yet!'" \;)
+		-exec bash -c "[ -f {}/reports/hls_verify.log ] || echo {}' not tested yet!'" \;) | sort
 	echo "------------------------------------"
 
 	echo 'MILP Runtimes'
 	find . -name 'buffers.log' -exec \
-		grep -H 'Total MILP' {} \;
+		grep -H 'Total MILP' {} \; | sort
 
 
 ) | tee results.log
