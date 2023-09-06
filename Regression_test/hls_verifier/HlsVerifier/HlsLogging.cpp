ca904f57652c92ef37b273e456a5d16fc3c8edea
Jiahui Xu <jxu@ethz.ch>
2023-09-06 18:17:15 +0200

    [HlsVerifier] now when HlsVerifier logs an ERROR message, make sure that it also to exit the program

--- Regression_test/hls_verifier/HlsVerifier/HlsLogging.cpp
+++ Regression_test/hls_verifier/HlsVerifier/HlsLogging.cpp
@@ -6,6 +6,7 @@ void log_inf(string tag, string msg) {
 
 void log_err(string tag, string msg) {
     cerr << "[ERROR " << tag << "] "  << msg << endl;
+    abort();
 }
 
 void log_wrn(string tag, string msg) {
