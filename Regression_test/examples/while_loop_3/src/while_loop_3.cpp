0999823e033dc99043ab1e60b3d1f5ffdefe0857
Jiahui Xu <jxu@ethz.ch>
2023-09-06 17:59:21 +0200

    [Benchmarks] fix while_loop_3 (not compatible with the HlsVerifier)

--- Regression_test/examples/while_loop_3/src/while_loop_3.cpp
+++ Regression_test/examples/while_loop_3/src/while_loop_3.cpp
@@ -7,25 +7,34 @@
 #include "while_loop_3.h"
 
 #define AMOUNT_OF_TEST 1
+#define BOUND 1000
 
-void while_loop_3 (out_int_t a[1]) {
+
+void while_loop_3 (out_int_t a[ARRAY_SIZE], in_int_t bound) {
 	int i = 0;
-	int bound = 1000;
 	int sum = 0;
 
-	while (i*i < 1000) {
+	while (i*i < bound) {
 		i++;
-	
+
 	}
-a[0] = i;
+	a[0] = i;
 }
 
 int main(void){
-	out_int_t a[AMOUNT_OF_TEST][1];
-    
+	out_int_t a[AMOUNT_OF_TEST][ARRAY_SIZE];
+	in_int_t bound[AMOUNT_OF_TEST];
+
 	//for(int i = 0; i < AMOUNT_OF_TEST; ++i){
-		int i = 0;
-		while_loop_3(a[i]);
+	for (int i = 0; i< AMOUNT_OF_TEST; i++){
+		bound[i] = 1;
+		for ( int j = 0; j < ARRAY_SIZE; j++) {
+			a[i][j] = 0;
+		}
+	}
+
+	int i = 0;
+	while_loop_3(a[i], bound[i]);
 	//}
 }
 
