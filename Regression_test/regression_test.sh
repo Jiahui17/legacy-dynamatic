878c70c8b81c5872b3d74325060c3e0881b392cc
Jiahui Xu <jxu@ethz.ch>
2023-09-07 13:42:12 +0200

    [Regression Test] increase the timeout for a single experiment

--- Regression_test/regression_test.sh
+++ Regression_test/regression_test.sh
@@ -11,7 +11,7 @@ RUN_DHLS="bash $SCRIPT_DIR/run_dhls.sh"
 
 # each example has up to 1000s (synthesis and simulation)
 # it is timed out after this duration
-TIMEOUT_DURATION='1000s'
+TIMEOUT_DURATION='1500s'
 
-xargs -r -i -a filelist.lst -n 1 -P 3 \
+xargs -r -i -a filelist.lst -n 1 -P 4 \
 	bash -c "cd ./examples/{} && timeout $TIMEOUT_DURATION $RUN_DHLS"
