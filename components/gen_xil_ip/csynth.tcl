f79089a5a20d8b31be589e5cae286911a9a4879b
Jiahui Xu <jxu@ethz.ch>
2023-10-01 09:26:53 +0200

    [fp wrappers] make explicit which target that the float units are generated from.

--- components/gen_xil_ip/csynth.tcl
+++ components/gen_xil_ip/csynth.tcl
@@ -1,12 +1,12 @@
 # generate wrappers for floating point units we still need to add additional
 # wrapper to make them elastic
-open_project -reset prj_dynamatic_units
-set_top dynamatic_units
-add_files dynamatic_units.c
-add_files -tb dynamatic_units.c
+open_project -reset prj_dynamatic_units_6ns
+set_top dynamatic_units_6ns
+add_files dynamatic_units_6ns.c
+add_files -tb dynamatic_units_6ns.c
 open_solution -reset "solution"
 set_part {xc7k160tfbg484-1}
-create_clock -period 4.000 -name default
+create_clock -period 6.000 -name default
 csim_design
 csynth_design
 cosim_design -rtl vhdl
